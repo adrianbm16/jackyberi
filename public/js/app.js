2fe9ca69de643c629f81d63f43463aaa34f0d907
adrianbm16 <adrianbernalmolina@gmail.com>
2025-05-07 11:20:14 +0200

    css nav

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,22 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> {{ $title ?? 'jackyberi' }} </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <header>
         <a href="/"><img class="logo" src="{{ asset('images/logo.png') }}" alt="Logo"></a>
-        <button class="menu-toggle" id="menu-toggle">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-        </button>
-        <nav>
-            <button class="close-menu" id="close-menu">
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </button>
-            <ul>
+        <button class="abrir-menu" id="abrir"><i class="bi bi-list"></i></button>
+        <nav class="nav" id="nav">
+            <button class="cerrar-menu" id="cerrar"><i class="bi bi-x"></i></button>
+            <ul class="nav-list">
                 <li><a href="/">Home</a></li>
                 <li><a href="/gallery">Gallery</a></li>
                 <li><a href="/contact">Contact</a></li>
@@ -32,5 +26,6 @@
 
     {{ $slot }}
 
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>

b78c97920aa03abb7e2888be545b5e14c228572d
adrianbm16 <adrianbernalmolina@gmail.com>
2025-05-09 12:31:04 +0200

    Conexion CSS con cada pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,10 @@
     
     <x-slot:title> Home </x-slot> <!-- Titulo de la pagina -->
 
+    <x-slot:styles> <!-- Estilos de la pagina -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}">
+    </x-slot>
+
     <div class="content">
         <h1>Welcome to the Home Page</h1>
         <p>This is the content of the home page.</p>
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -2,6 +2,10 @@
     
     <x-slot:title> Shop </x-slot> <!-- Titulo de la pagina -->
 
+    <x-slot:styles> <!-- Estilos de la pagina -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/shop.css') }}">
+    </x-slot>
+
     <div class="content">
         <h1>Welcome to the Shop</h1>
         <p>This is the content of the shop page.</p>

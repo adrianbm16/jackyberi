865ee2062503428d086627a435e9bbd2b9cbcf0a
adrianbm16 <adrianbernalmolina@gmail.com>
2025-05-26 11:40:42 +0200

    Cambios de sitio de codigo de archivos generales

--- public/js/app.js
+++ public/js/app.js
@@ -59,39 +59,5 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// -------------------------------------------- GALLERY --------------------------------------------
-/**
- * Este codigo permite abrir un modal al hacer clic en una imagen de la galeria.
- * El modal muestra la imagen ampliada y un caption con el texto alternativo de la imagen.
- * Al hacer clic en el boton de cerrar o fuera de la imagen, el modal se cierra.
- * @param modal - El modal de la galeria.
- * @param modalImg - La imagen del modal.
- * @param captionText - El texto del caption.
- * @param closeBtn - El boton de cerrar el modal.
- */
-// Obtener elementos del modal
-const modal = document.getElementById("imageModal");
-const modalImg = document.getElementById("modalImg");
-const captionText = document.getElementById("caption");
-const closeBtn = document.querySelector(".close");
 
-// Agregar evento a cada imagen
-document.querySelectorAll(".gallery-img").forEach(img => {
-    img.addEventListener("click", function () {
-        modal.style.display = "block"; // Mostrar el modal
-        modalImg.src = this.src; // Establecer la imagen en el modal
-        captionText.innerHTML = this.alt; // Establecer el texto del caption
-    });
-});
 
-// Cerrar el modal al hacer clic en el botón de cerrar
-closeBtn.onclick = function () {
-    modal.style.display = "none";
-};
-
-// Cerrar el modal al hacer clic fuera de la imagen
-modal.onclick = function (e) {
-    if (e.target === modal) {
-        modal.style.display = "none";
-    }
-};
